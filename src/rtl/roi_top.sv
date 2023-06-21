04653529e3d60ed0254cedff2769233b66840b84
Artem Serbin <lil7.jooob@mail.ru>
2023-06-21 11:53:08 +0300

    initial

---
+++ src/rtl/roi_top.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.06.2023 11:42:47
+// Design Name: 
+// Module Name: roi_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module roi_top(
+
+    );
+endmodule
---
+++ src/tb/roi_axis_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.06.2023 11:46:06
+// Design Name: 
+// Module Name: roi_axis_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module roi_axis_tb(
+
+    );
+endmodule
