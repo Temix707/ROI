cac725cec022909cd31fc9244bf963886c79ff68
Artem Serbin <lil7.jooob@mail.ru>
2023-07-09 20:28:38 +0300

    feat (rtl, tb): изменение дизайна
    Был изменена логика дизайна: был убран конечный автомат, исправлена система нахождения координат и подсчет счетчиков, в следствии чего данные теперь передаются корректно.
    Так же был изменен тестовое окружение под новый дизайн и добавление проверки с несколькими изображениями.

--- src/rtl/roi_axis.sv
+++ src/rtl/roi_axis.sv
@@ -26,40 +26,36 @@ module roi_axis
   localparam X0_L = 0;
   localparam X0_R = 1;
 
-  logic [$clog2( HEIGHT * WIDTH )-1:0]  cnt_quan_pxl;                     //  data quantity counter (log2 (480000) = 19 bit)
+  logic [$clog2( HEIGHT * WIDTH ) - 1:0]  cnt_quan_pxl;                     //  data quantity counter (log2 (480000) = 19 bit)
 
-  logic [$clog2( WIDTH  )-1:0]          cnt_last_val;
+  logic [$clog2( WIDTH  ) - 1:0]          cnt_last_val;
 
   //  X (width) and Y (height) coordinate counters for a LARGE area
-  logic [$clog2( WIDTH  )-1:0]          cnt_l_x;
-  logic [$clog2( HEIGHT )-1:0]          cnt_l_y;
+  logic [$clog2( WIDTH  )-1:0]            cnt_l_x;
+  logic [$clog2( HEIGHT )-1:0]            cnt_l_y;
 
   // X (width) and Y (height) coordinate counters for a SMALL area (to check) and also counts all pixels in a small area
-  logic [$clog2( HEIGHT * WIDTH )-1:0]  cnt_s_x_pxl;
-  //logic [$clog2( HEIGHT )-1:0]        cnt_s_y; 
+  logic [$clog2( HEIGHT * WIDTH ) - 1:0]  cnt_s_x_pxl;
 
   // Coordinate buffers
-  logic [9:0]                           x0, y0;
-  logic [9:0]                           x1, y1;
+  logic [9:0]                             x0, y0;
+  logic [9:0]                             x1, y1;
 
   // Buffers for finding points
-  logic                                 find_xy0_l, find_xy1_r;
-  logic                                 find_xy0_r, find_xy1_l;
+  logic                                   find_xy0_l, find_xy1_r;
+  logic                                   find_xy0_r, find_xy1_l;
 
-  // Counters of fullness and emptiness
-  logic                                 wr_full, rd_empty;
+  logic                                   wr_full, rd_empty;                // Counters of fullness and emptiness
 
-  // Registers for input data (remove the delay)
-  logic [BIT_DATA_O-1:0]                data_ff_1, data_ff_2, data_ff_3;
-
-  // Verification data
-  logic [BIT_DATA_O-1:0]                data_check;
+  logic [BIT_DATA_O-1:0]                  data_check;                       // Verification data
+  
+  logic [$clog2( HEIGHT * WIDTH ) - 1:0]  SUM_PIX_SMALL_AREA;               // Small area size
 
 
   always_comb begin
     // Full block and empty block counters
-    wr_full   = ( cnt_quan_pxl == ((HEIGHT) * (WIDTH)) );         // HEIGHT * WIDTH = 600 * 800 = 480_000 data
-    rd_empty  = ( cnt_quan_pxl == 0 );
+    wr_full   = ( cnt_quan_pxl == ((HEIGHT) * (WIDTH)) ) ? 1'b1 : 1'b0;     // HEIGHT * WIDTH = 600 * 800 = 480_000 data
+    rd_empty  = ( cnt_quan_pxl == 0 )                    ? 1'b1 : 1'b0;
 
     // Assigning coordinates
     x0        = xy_0_i[26:16];
@@ -71,64 +67,22 @@ module roi_axis
 
   // Finding points
   always_comb begin
-    if ( x0 < x1 ) begin                                                  // if the point xy0 is to the left of the point xy1
+    if ( x0 < x1 ) begin                                                    // if the point xy0 is to the left of the point xy1
       find_xy0_l  = ( cnt_l_x == x0 ) &&  ( cnt_l_y == y0 );
       find_xy1_r  = ( cnt_l_x == x1 ) &&  ( cnt_l_y == y1 );
     end 
-    else begin                                                            // if point xy1 is to the left of point xy0
+    else begin                                                              // if point xy1 is to the left of point xy0
       find_xy0_r  = ( cnt_l_x == x0 ) &&  ( cnt_l_y == y0 );
-      find_xy1_l  = ( cnt_l_x >= x1 ) &&  ( cnt_l_y >= y1 );              // Из-за того, что x0 находился правее, дописал знак (>=) для того, чтобы счетчик  
-    end                                                                   // досчитывал до последних данных в маленькой области и отправлял сигнал tlast_o.
+      find_xy1_l  = ( cnt_l_x >= x1 ) &&  ( cnt_l_y >= y1 );                // Из-за того, что x0 находился правее, дописал знак (>=) для того, чтобы счетчик  
+    end                                                                     // досчитывал до последних данных в маленькой области и отправлял сигнал tlast_o.
   end
 
 
-  typedef enum logic [0:0] { 
-                      IDLE      = 0,
-                      AREA_PH   = 1     
-  }  type_state;
-   
-  type_state state, next_st;
-   
-
-
-  ////////////////////////////////////////
-  // States of the finite state machine //
-  ////////////////////////////////////////
-
-  always_ff @( posedge clk_i or posedge arst_i ) begin
-    if( arst_i )                          state <= IDLE;    
-    else                                  state <= next_st;
-  end
-
-
-  always_comb begin
-    next_st = IDLE;
-
-    case( state )
-      IDLE: begin
-        // Checking coordinates beyond the limit of a large area
-        if( !(( x0 > WIDTH ) || ( y0 > HEIGHT ) || ( x1 > WIDTH ) || ( y1 > HEIGHT )) ) begin   
-          if( tvalid_i && !tlast_i )      next_st = AREA_PH;
-          else                            next_st = IDLE;
-        end
-        else begin
-                                          next_st = IDLE;
-        end
-      end
-
-      AREA_PH: begin
-          if( !( tvalid_i && !tlast_i ) ) next_st = IDLE;
-          else                            next_st = AREA_PH;
-      end
-
-      default:                            next_st = IDLE;
-    endcase
-  end
 
 
 
   /////////////////////////////////////////////
-  // Logic of a combinational finite machine //
+  ///////////  Combinational logic  ///////////
   /////////////////////////////////////////////
 
   always_comb begin    
@@ -138,14 +92,15 @@ module roi_axis
 
     data_check  = 0;
 
-    case ( state )
-      AREA_PH:  begin
-        data_check = data_ff_3;
+    if( !(( x0 > WIDTH ) || ( y0 > HEIGHT ) || ( x1 > WIDTH ) || ( y1 > HEIGHT )) ) begin 
+      if( (tvalid_i && !tlast_i) || (tvalid_i && tlast_i) ) begin
+        data_check = tdata_i;
         case( x0 > x1 )
           X0_L: begin
+            SUM_PIX_SMALL_AREA = (( ( x1 - x0 ) + 1 ) * ( ( y1 - y0 ) + 1 )); 
             if( ( cnt_l_x > ( x0 - 1 ) ) && ( cnt_l_x < ( x1 + 1 ) ) && ( cnt_l_y > ( y0 - 1 ) ) && ( cnt_l_y < ( y1 + 1 ) )) begin
               tvalid_o    = tvalid_i;
-              tdata_o     = data_ff_3;
+              tdata_o     = tdata_i;
             end
             else begin
               tvalid_o    = 0;
@@ -158,7 +113,8 @@ module roi_axis
           end
 
           X0_R: begin
-            if( ( cnt_l_x <= ( x0 - 1 ) ) && ( cnt_l_x >= ( x1 ) ) && ( cnt_l_y > ( y0 - 1 ) ) && ( cnt_l_y < ( y1 + 1 ) )) begin
+            SUM_PIX_SMALL_AREA = (( ( x0 - x1 ) + 1 ) * ( ( y1 - y0 ) + 1));
+            if( ( cnt_l_x < ( x0 + 1 ) ) && ( cnt_l_x > ( x1 - 1 ) ) && ( cnt_l_y > ( y0 - 1 ) ) && ( cnt_l_y < ( y1 + 1 ) )) begin
               tvalid_o    = tvalid_i;
               tdata_o     = tdata_i;
             end
@@ -175,35 +131,25 @@ module roi_axis
           end
         endcase
       end
-
-    endcase
-  end
-
-
-
-  always_ff @( posedge clk_i or posedge arst_i ) begin
-    if( tvalid_i && !tlast_i ) begin
-      data_ff_1   <= tdata_i;
-      data_ff_2   <= data_ff_1;
-      data_ff_3   <= data_ff_2;
     end
   end
 
 
+
+
   ////////////////////////////////////////////
-  // Sequential logic of a finite automaton //
+  ////////////  Sequential logic  ////////////
   ////////////////////////////////////////////
 
   always_ff @( posedge clk_i or posedge arst_i ) begin
     if( arst_i ) begin 
       cnt_l_x       <= 0;   cnt_l_y       <= 1;
-      cnt_s_x_pxl   <= 0;
+      cnt_s_x_pxl   <= 1;
       cnt_quan_pxl  <= 0;   cnt_last_val  <= 0;
-      data_ff_1     <= 0;   data_ff_2     <= 0;   data_ff_3 <= 0;
     end
     else begin
-      case( state )
-        AREA_PH:  begin
+      if( !(( x0 > WIDTH ) || ( y0 > HEIGHT ) || ( x1 > WIDTH ) || ( y1 > HEIGHT )) ) begin 
+        if( (tvalid_i && !tlast_i) || (tvalid_i && tlast_i) ) begin
 
           /////////////////////////////////////
           //////// Large area counters ////////
@@ -231,50 +177,59 @@ module roi_axis
           if( cnt_quan_pxl == ( HEIGHT * WIDTH ) ) begin
             cnt_quan_pxl  <= 0;
             cnt_l_x       <= 0;
-            cnt_l_y       <= 0;
+            cnt_l_y       <= 1;
           end
-
 
 
           /////////////////////////////////////
           ////// Selections a small area //////
           /////////////////////////////////////
 
-            case( x0 > x1 )
-              X0_L: begin
-                if( ( cnt_l_x > ( x0 - 1 ) ) && ( cnt_l_x < ( x1 + 1 ) ) && ( cnt_l_y > ( y0 - 1 ) ) && ( cnt_l_y < ( y1 + 1 ) )) begin
-                  cnt_s_x_pxl <= cnt_s_x_pxl + 1;              
-                end
+          case( x0 > x1 )
+            X0_L: begin
+              if( ( cnt_l_x > ( x0 - 1 ) ) && ( cnt_l_x < ( x1 + 1 ) ) && ( cnt_l_y > ( y0 - 1 ) ) && ( cnt_l_y < ( y1 + 1 ) )) begin
+                if( !( cnt_s_x_pxl == SUM_PIX_SMALL_AREA ) ) begin
+                  cnt_s_x_pxl   <= cnt_s_x_pxl + 1;     
+                end       
                 else begin
-                  cnt_s_x_pxl     <= cnt_s_x_pxl;
+                  cnt_s_x_pxl   <= 1;
                 end
               end
+              else begin
+                cnt_s_x_pxl     <= cnt_s_x_pxl;
+              end
+            end
 
-
-              X0_R: begin
-                if( ( cnt_l_x <= ( x0 - 1 ) ) && ( cnt_l_x >= ( x1 ) ) && ( cnt_l_y > ( y0 - 1 ) ) && ( cnt_l_y < ( y1 + 1 ) )) begin
+            X0_R: begin
+              if( ( cnt_l_x < ( x0 + 1 ) ) && ( cnt_l_x > ( x1 - 1 ) ) && ( cnt_l_y > ( y0 - 1 ) ) && ( cnt_l_y < ( y1 + 1 ) )) begin
+                if( !( cnt_s_x_pxl == SUM_PIX_SMALL_AREA ) ) begin
                   cnt_s_x_pxl     <= cnt_s_x_pxl + 1;
                 end
                 else begin
-                  cnt_s_x_pxl     <= cnt_s_x_pxl;
+                  cnt_s_x_pxl     <= 1;
                 end
-
-                if( find_xy1_l) begin
-                  if( cnt_last_val == ( x0 - x1 ) ) begin
-                    cnt_last_val  <= 0;
-                  end
-                  else begin
-                    cnt_last_val  <= cnt_last_val + 1;
-                  end
-                end
-
               end
-            endcase
-          end
-      endcase
+
+              if( find_xy1_l ) begin
+                if( cnt_last_val == ( x0 - x1 ) ) begin
+                  cnt_last_val  <= 0;
+                end
+                else begin
+                  cnt_last_val  <= cnt_last_val + 1;
+                end
+              end
+            end
+          endcase
+
+        end
+      end
+      else begin
+        cnt_l_x       <= 0;   cnt_l_y       <= 1;
+        cnt_s_x_pxl   <= 1;
+        cnt_quan_pxl  <= 0;   cnt_last_val  <= 0;
+      end
+
     end
   end
   
-
 endmodule
-
--- src/tb/roi_axis_tb.sv
+++ src/tb/roi_axis_tb.sv
@@ -13,6 +13,7 @@ class trans;
   endfunction
 endclass
 
+
 module roi_axis_tb
 #(
   parameter               WIDTH       = 800,                 
@@ -60,48 +61,52 @@ module roi_axis_tb
   logic [9:0] x0, y0;
   logic [9:0] x1, y1;
 
+  always_comb begin
+    x0 = xy_0_i[26:16];
+    y0 = xy_0_i[9:0];
+
+    x1 = xy_1_i[26:16];
+    y1 = xy_1_i[9:0];
+  end
+
+
+  // Small area size
+  logic [BIT_COORD - 1:0] SUM_PIX_SMALL_AREA;                       // example: 201 * 401 = 80601 
+
+  always_comb begin
+    if( x0 < x1 ) SUM_PIX_SMALL_AREA = (( ( x1 - x0 ) + 1 ) * ( ( y1 - y0 ) + 1 ));  
+    else          SUM_PIX_SMALL_AREA = (( ( x0 - x1 ) + 1 ) * ( ( y1 - y0 ) + 1 ));
+  end
+
+
   // CLK
   initial begin
     clk_i  = '0;
-    forever #10 clk_i  = ~clk_i;
+    forever #10 clk_i = ~clk_i;
   end
 
-  // RESET
+  
+  trans pkt;
+
   initial begin
+    pkt = new();
+    
+    // First image
+    xy_0_i[31:0]  = { 6'd0, 10'd400, 6'd0, 10'd300 };               // [26:16] x0, [9:0] y0  (400, 300)
+    xy_1_i[31:0]  = { 6'd0, 10'd600, 6'd0, 10'd400 };               // [26:16] x1, [9:0] y1  (600, 400)
+
+    // RESET
     arst_i = '1;
     repeat (2)  @ ( posedge clk_i );
     arst_i = '0;
-  end
 
-
-  // Set coordinates to points 
-  initial begin                                   
-    xy_0_i[31:0]  = { 6'd0, 10'd200, 6'd0, 10'd200 };    // [26:16] x0, [9:0] y0  (200,200)
-    xy_1_i[31:0]  = { 6'd0, 10'd600, 6'd0, 10'd400 };    // [26:16] x1, [9:0] y1  (600,400)
-
-    x0            = xy_0_i[26:16];
-    y0            = xy_0_i[9:0];
-
-    x1            = xy_1_i[26:16];
-    y1            = xy_1_i[9:0];
-  end
-
-
-
-
-
-  trans pkt;
-
-  initial begin 
-    pkt = new();
-
-    repeat (2)  @ ( posedge clk_i );
     tlast_i       = 0;
     tvalid_i      = 0;
 
     repeat (2)  @ ( posedge clk_i );
     tlast_i       = 0;
     tvalid_i      = 1;
+
 
     repeat (5)  @ ( posedge clk_i );
     tlast_i       = 0;
@@ -111,22 +116,44 @@ module roi_axis_tb
     tlast_i       = 0;
     tvalid_i      = 1;
 
+    // Second image
+    repeat ( (WIDTH * HEIGHT) + 10000 ) @ ( posedge clk_i );
+    tlast_i       = 0;
+    tvalid_i      = 1;
+    xy_0_i[31:0]  = { 6'd0, 10'd200, 6'd0, 10'd200 };               // [26:16] x0, [9:0] y0  (200, 200)
+    xy_1_i[31:0]  = { 6'd0, 10'd600, 6'd0, 10'd400 };               // [26:16] x1, [9:0] y1  (600, 400)
+
+
+    // Third image
+    repeat ( (WIDTH * HEIGHT) + 10000 ) @ ( posedge clk_i );
+    tlast_i       = 0;
+    tvalid_i      = 1;
+    xy_0_i[31:0]  = { 6'd0, 10'd700, 6'd0, 10'd100 };               // [26:16] x0, [9:0] y0  (700, 100)
+    xy_1_i[31:0]  = { 6'd0, 10'd400, 6'd0, 10'd500 };               // [26:16] x1, [9:0] y1  (400, 500)
+
+    // Fourth image
+    repeat ( (WIDTH * HEIGHT) + 10000 ) @ ( posedge clk_i );
+    tlast_i       = 0;
+    tvalid_i      = 1;
+    xy_0_i[31:0]  = { 6'd0, 10'd1, 6'd0, 10'd1     };               // [26:16] x0, [9:0] y0  (1,1)
+    xy_1_i[31:0]  = { 6'd0, 10'd800, 6'd0, 10'd600 };               // [26:16] x1, [9:0] y1  (800, 600)
+
+    // Fifth image
+    repeat ( (WIDTH * HEIGHT) + 10000 ) @ ( posedge clk_i );
+    tlast_i       = 0;
+    tvalid_i      = 0;
+    xy_0_i[31:0]  = { 6'd0, 10'd200, 6'd0, 10'd300 };               // [26:16] x0, [9:0] y0  (200,300)
+    xy_1_i[31:0]  = { 6'd0, 10'd900, 6'd0, 10'd700 };               // [26:16] x1, [9:0] y1  (900, 700) - invalid coordinates
   end
 
 
 
-
-
-  // Small area size
-  logic [BIT_COORD - 1:0] SUM_PIX_SMALL_AREA;
-  assign SUM_PIX_SMALL_AREA = (( x1 - x0 + 1) * ( y1 - y0 + 1));  // 201 * 401 = 80601 values => 0...80600
-
-
-
-
+  ////////////////////////////////
   //// Randomization of data  ////
+  ////////////////////////////////
+
   always_ff @( posedge clk_i ) begin
-    if( tvalid_i && !tlast_i ) begin
+    if( (tvalid_i && !tlast_i) || (tvalid_i && tlast_i) ) begin
       pkt.randomize();
       tdata_i = pkt.random_val( pkt.pixel); 
       // pkt.print( tdata_i );
@@ -138,156 +165,101 @@ module roi_axis_tb
 
 
 
-  logic [BIT_DATA_O-1:0]    data_ff_1, data_ff_2, data_ff_3;
-  logic [BIT_DATA_O - 1:0]  data_show;
 
+  ///////////////////////////////////////////////
+  ///// Queue check block for a LARGE area  /////
+  ///////////////////////////////////////////////
+
+  logic [2:0]               cnt_l_img;
+         
+  logic [BIT_DATA_O - 1:0]  cnt_data_i_que [$];           // Queue for checking data in a large area
+  
   logic                     tlast_ff;
   logic                     tvalid_ff;
-
-
-  logic [BIT_DATA_O - 1:0]  data_i_que     [$];       // Queue for checking data in a large area   
-  logic [BIT_DATA_O - 1:0]  cnt_data_i_que [$]; 
   
-  logic [1:0]               cnt_delay;                // Input data delay counter
-
-
-  always_ff @( posedge clk_i or posedge arst_i ) begin
-    if( arst_i ) begin
-      cnt_data_i_que  = {};                         
-      cnt_delay       = 0;                                      
-    end
-    else begin
-      
-      if( tvalid_i ) begin
-        data_ff_1   <= tdata_i;
-        data_ff_2   <= data_ff_1;
-        data_ff_3   <= data_ff_2;
-        cnt_data_i_que.push_back( data_ff_2 );
-      end
-
-      // Avoid delays with the first data received (2 cycles)
-      if( !(cnt_delay == 1) ) begin
-        if( cnt_data_i_que.size() == 2 ) begin
-          cnt_data_i_que.delete();
-          cnt_delay = cnt_delay + 1;
-        end
-      end 
-
-      if( cnt_data_i_que.size() == SUM_PIX ) begin
-        $display  ( "ALL DATA WAS RECORDED OVER A LARGE AREA: \nIt should have come: %0d, \t Has come: %0d, \t Time: %0t \n"
-        , SUM_PIX, cnt_data_i_que.size(), $time );
-        
-        tvalid_ff = 0;
-        tlast_ff  = 1;
-
-        $stop();
-        cnt_data_i_que.delete();
-      end
-      else begin
-        $display  ( "Not all pixels were transferred to a large area. \nInp data: %0d, \tThe amount of data recorded: %0d, \t Time: %0t \n-----------------------"
-        , data_ff_2, cnt_data_i_que.size(), $time );
-      end
-
-    end
-  end
-
   assign tvalid_i = tvalid_ff;
   assign tlast_i  = tlast_ff;
 
-
-
-/*
-  ///// Queue check block for a large area  /////
   always_ff @( posedge clk_i or posedge arst_i ) begin
     if( arst_i ) begin
-      data_i_que  = {};                         
-      cnt_delay   = 0;                                      
+      cnt_data_i_que  = {};
+      cnt_l_img       = 1;                                                             
     end
     else begin
       if( tvalid_i ) begin
-        data_i_que.push_back( tdata_i );
+        tvalid_ff     = 1;
+        cnt_data_i_que.push_back( tdata_i );
       end
 
-      // Avoid delays with the first data received (2 cycles)
-      if( !(cnt_delay == 1) ) begin
-        if( data_i_que.size() == 2 ) begin
-          data_i_que.delete();
-          cnt_delay = cnt_delay + 1;
-        end
-      end      
-      
-      if( data_i_que.size() == SUM_PIX ) begin
-        $display  ( "ALL DATA WAS RECORDED OVER A LARGE AREA: \nIt should have come: %0d, \t Has come: %0d, \t Time: %0t \n"
-        , SUM_PIX, data_i_que.size(), $time );
-        //$stop();
-      end
-      else if( data_i_que.size() == 0 ) begin
-        $display  ( "There are no transmitted pixels" );
-      end
-      else if( data_i_que.size() > SUM_PIX ) begin
-        $display  ( "All pixels have already been transferred" );
+      if( cnt_data_i_que.size() == SUM_PIX ) begin
+        $display  ( "SUCCESS: ALL DATA WAS RECORDED IN %0d LARGE IMAGE: \nIt should have come: %0d, \tHas come: %0d, \tTime: %0t \n"
+        , cnt_l_img, SUM_PIX, cnt_data_i_que.size(), $time );
+        
+        cnt_l_img     = cnt_l_img + 1;
+        tlast_ff      = 1;
       end
       else begin
-        $display  ( "Not all pixels were transferred to a large area. \nInp data: %0d, \tThe amount of data recorded: %0d, \t Time: %0t \n-----------------------"
-                  , tdata_i, data_i_que.size(), $time );
+        tlast_ff      = 0;
       end
-      
+
+
+      if( cnt_data_i_que.size() == SUM_PIX + 1 ) begin
+        tvalid_ff     = 0;
+        cnt_data_i_que.delete();
+        //$stop();
+      end
+      /*else begin
+        $display  ( "Not all pixels were transferred to a large area. \nInp data: %0d, \tThe amount of data recorded: %0d, \t Time: %0t \n-----------------------"
+        , tdata_i, cnt_data_i_que.size(), $time );
+      end*/
     end
   end
-  ////////////////////////////////////////////////  
-*/
 
 
 
 
 
 
-  ///// A queue check block for a small area /////
-  logic [BIT_DATA_O - 1:0] data_o_que [$];
+  ////////////////////////////////////////////////
+  ///// A queue check block for a SMALL area /////
+  ////////////////////////////////////////////////
+
+  logic [2:0]              cnt_s_img;
+
   logic [BIT_DATA_O - 1:0] cnt_data_o_que [$];
-
   logic [BIT_DATA_O - 1:0] down_data_expected;   
 
   
   always_ff @( posedge clk_i or posedge arst_i) begin
     if( arst_i ) begin
-      data_o_que      = {}; 
-      cnt_data_o_que  = {};
+      cnt_data_o_que = {};
+      cnt_s_img      = 1;
     end
     else begin
-
       if( tvalid_o ) begin
-        cnt_data_o_que.push_back(tdata_i);
+        cnt_data_o_que.push_back( tdata_i );
 
-        data_o_que.push_back(tdata_i);
-        down_data_expected <= data_o_que.pop_back();
-      end
+        /*down_data_expected <= tdata_i;
 
-     /* if( down_data_expected == tdata_o ) begin
-        $display  ( "SUCCESS: The data is transmitted correctly: \nThe data is not transmitted correctly: %0d, \tOutput data from a small area: %0d, \tTime: %0t\n-----------------------"
-        , down_data_expected, tdata_o, $time );*/
+        if( down_data_expected == tdata_o ) begin
+          $display  ( "SUCCESS: The data is transmitted correctly: \nInput data to a small area: %0d, \tOutput data from a small area: %0d, \tTime: %0t\n-----------------------"
+          , down_data_expected, tdata_o, $time );
+        end
+        else begin
+          $display  ( "ERROR: The data is transmitted incorrectly: \nInput data to a small area: %0d, \tOutput data from a small area: %0d, \tTime: %0t\n-----------------------"
+          , down_data_expected, tdata_o, $time );
+        end*/
 
         if( cnt_data_o_que.size() == SUM_PIX_SMALL_AREA ) begin
-            $display  ( "SUCCESS: ALL DATA FROM A SMALL AREA WAS TRANSMITTED: \nIt should have come: %0d, Has come: %0d, \tTime: %0t \n-----------------------"
-            , SUM_PIX_SMALL_AREA, cnt_data_o_que.size() , $time );
+            $display  ( "SUCCESS: ALL DATA FROM THE %0d SMALL IMAGE WAS TRANSMITTED: \nIt should have come: %0d, Has come: %0d, \tTime: %0t \n-----------------------"
+            , cnt_s_img, SUM_PIX_SMALL_AREA, cnt_data_o_que.size() , $time );
+        
+            cnt_s_img = cnt_s_img + 1;
             cnt_data_o_que.delete();
             //$stop();
         end
-        /*else begin
-          $display  ( "ERROR: Not all data from a small area was transmitted: \nIt should have come: %0d, Has come: %0d, \tTime: %0t \n-----------------------"
-          , SUM_PIX_SMALL_AREA, cnt_data_o_que.size() , $time );
-        end*/
-     // end
-     /* else begin
-        $display  ( "ERROR: Small area not found. \nThe data is not transmitted correctly: \nInput data for a small area: %0d, \tOutput data for a small area: %0d, \tTime: %0t\n-----------------------"
-        , down_data_expected, tdata_o, $time );
-      end*/
-
+      end
     end
   end
-  ////////////////////////////////////////////////
-
-
-
 
 endmodule
