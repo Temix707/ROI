6cdedd57f59f0a0c48738986ddc654f68efaf2c2
Artem Serbin <lil7.jooob@mail.ru>
2023-07-11 00:18:11 +0300

    feat (rtl, tb): добавление измененного дизайна шины apb
    Был добавлен начальный изменный дизайн и тестовое окружение шины apb.

--- src/rtl/roi_apb.sv
+++ src/rtl/roi_apb.sv
@@ -1,60 +1,166 @@
 module roi_apb
 #(
-  parameter                             APB_DATA_I_WIDTH = 64,
-                                        APB_DATA_O_WIDTH = 32,
-
-                                        APB_ADDR_WIDTH   = 12     // APB slaves are 4KB by default 
+  parameter                       APB_DATA_W = 32,
+                                  APB_ADDR_W = 12 
 )
 (
-  input   logic                         clk_i,
-  input   logic                         arst_i,
+  input   logic                   clk_i,
+  input   logic                   arst_i,
 
-  input   logic [APB_DATA_I_WIDTH-1:0]  apb_pwdata_i,                
-  input   logic [APB_ADDR_WIDTH-1:0]    apb_paddr_i,                   
+  input   logic [APB_DATA_W-1:0]  apb_pwdata_i,                
+  input   logic [APB_ADDR_W-1:0]  apb_paddr_i,                   
   
-  input   logic                         apb_pwrite_i,             // Always in the recording state
-  input   logic                         apb_psel_i,                   
+  input   logic                   apb_pwrite_i,            
+  input   logic                   apb_psel_i,
+  input   logic                   apb_penable_i,     
 
-  output  logic  [APB_DATA_O_WIDTH-1:0] xy_0_o,
-  output  logic  [APB_DATA_O_WIDTH-1:0] xy_1_o
+  output  logic                   apb_pready_o,                   
+  output  logic  [APB_DATA_W-1:0] apb_prdata_o,   
+  
+  output  logic  [APB_DATA_W-1:0] xy_0_o,
+  output  logic  [APB_DATA_W-1:0] xy_1_o
 );
 
-  // Local declarations
-  localparam ADDR_ROI = 12'h0;                                    // Slave Device Address (ROI)
 
-  logic                         apb_write;
-  logic                         apb_sel_roi_val;
+  localparam ADDR_XY_0    = 12'h0;
+  localparam ADDR_XY_1    = 12'h4;
 
-  logic [APB_DATA_I_WIDTH-1:0]  xy_val_ff;  
 
-  // APB decoding
+  logic                   apb_write;
+  logic                   apb_read;
+
+
+  // Address selection
+  logic                   apb_sel_xy_0_o;
+  logic                   apb_sel_xy_1_o;
+
+
+  // Registers
+  // Input data
+  logic [APB_DATA_W-1:0]  data_i_ff;
+  logic [APB_DATA_W-1:0]  data_i_next;
+  logic                   data_i_en;
+
+  // Output coordinates xy0
+  logic [APB_DATA_W-1:0]  data_xy_0_o_ff;
+  //logic [APB_DATA_W-1:0]  data_xy_0_o_next;
+  //logic                   data_xy_0_o_en;     ?
+
+  // Output coordinates xy1
+  logic [APB_DATA_W-1:0]  data_xy_1_o_ff;
+  //logic [APB_DATA_W-1:0]  data_xy_1_o_next;
+  //logic                   data_xy_1_o_en;     ?
+
+  // PRDATA
+  logic [APB_DATA_W-1:0]  apb_dout_ff;
+  logic [APB_DATA_W-1:0]  apb_dout_next;
+  logic                   apb_dout_en;
+
+  //  PREADY
+  logic                   apb_ready_ff;
+  logic                   apb_ready_next;
+  logic                   apb_ready_en;
+
+
+
+  //////////////////////////
+  /////  APB decoding  /////
+  //////////////////////////
+
   always_comb begin
-    apb_write       = apb_psel_i & apb_pwrite_i;
+    apb_write      =   apb_psel_i &  apb_pwrite_i;
+    apb_read       =   apb_psel_i & ~apb_pwrite_i;
 
-    apb_sel_roi_val = ( apb_paddr_i == ADDR_ROI );                // Choosing a peripheral device (ROI)
+    apb_sel_xy_0_o = ( apb_paddr_i == ADDR_XY_0  );
+    apb_sel_xy_1_o = ( apb_paddr_i == ADDR_XY_1  );
   end
 
 
-  // APB data out
-  logic [APB_DATA_I_WIDTH-1:0] apb_dout_ff;
-  logic [APB_DATA_I_WIDTH-1:0] apb_dout_next;
 
-  // Input reg values xy
+
+  //////////////////////////
+  ///  Data in register  ///
+  //////////////////////////
+
+  // Data in
+
+  assign data_i_en = apb_write;
+
+  assign data_i_next = apb_pwdata_i;
+
   always_ff @( posedge clk_i or posedge arst_i ) begin
-    if ( arst_i ) xy_val_ff   <= '0;
-    else          xy_val_ff   <= apb_pwdata_i;
+    if    ( arst_i    ) data_i_ff <= '0;
+    else  ( data_i_en ) data_i_ff <= data_i_next;
   end
 
-  // Output reg values
+
+
+
+  //////////////////////////
+  /////  APB data out  /////
+  //////////////////////////
+
+  //  PRDATA  //
+
+  assign apb_dout_en   = apb_read;
+
+  assign apb_dout_next = apb_sel_xy_0_o ? APB_DATA_W'( data_i_ff )
+                       : apb_sel_xy_1_o ? APB_DATA_W'( data_i_ff )   
+                       :                  '0;
+
+
   always_ff @( posedge clk_i or posedge arst_i ) begin
-    if ( arst_i ) apb_dout_ff <= '0;
-    else          apb_dout_ff <= apb_dout_next;
+    if      ( arst_i      )   apb_dout_ff <= '0;
+    else if ( apb_dout_en )   apb_dout_ff <= apb_dout_next;
   end
 
-  assign apb_dout_next = apb_sel_roi_val ? ( xy_val_ff ) : ( '0 );
+  assign apb_prdata_o  = apb_dout_ff;
 
-  assign xy_0_o        = apb_dout_ff [31:0];
-  assign xy_1_o        = apb_dout_ff [63:32];
+
+
+  //  XY0  //
+
+  always_ff @( posedge clk_i or posedge arst_i ) begin
+    if      ( arst_i )    data_xy_0_o_ff <= '0;
+    else /*if ( ? ) */    data_xy_0_o_ff <= apb_dout_next;
+  end
+
+  assign xy_0_o = data_xy_0_o_ff;
+
+
+
+
+  //  XY1  //
+
+  always_ff @( posedge clk_i or posedge arst_i ) begin
+    if      ( arst_i )    data_xy_1_o_ff <= '0;
+    else /*if ( ? ) */    data_xy_1_o_ff <= apb_dout_next;
+  end
+
+  assign xy_1_o = data_xy_0_o_ff;
+
+
+
+
+
+
+  ///////////////////////////
+  //////   APB ready   //////
+  ///////////////////////////
+
+  assign apb_ready_en   = ( apb_psel_i & apb_penable_i ) | apb_ready_ff;
+
+  assign apb_ready_next = ( apb_psel_i & apb_penable_i ) & ~apb_ready_ff;
+
+
+  always_ff @( posedge clk_i or posedge arst_i ) begin
+    if      ( arst_i       )    apb_ready_ff <= '0;
+    else if ( apb_ready_en )    apb_ready_ff <= apb_ready_next;
+  end
+
+  assign apb_pready_o  = apb_ready_ff;
+
+
 
 endmodule
 
@@ -74,10 +180,56 @@ endmodule
 
 
 
-/*    
-  input  logic                          apb_penable_i,              // (2d cycle)
 
-  output logic [BIT_C-1:0]              apb_prdata_o,
-  output logic                          apb_pready_o,
-  output logic                          apb_pslverr_o,
+
+
+///////////////////////////////////////////////////
+/////////////////   OLD  //////////////////////////
+///////////////////////////////////////////////////
+
+/*
+  // Local declarations
+  localparam ADDR_ROI = 12'h0;                                    // Slave Device Address (ROI)
+
+  logic                         apb_write;
+  logic                         apb_sel_roi_val;
+
+  logic [APB_DATA_I_WIDTH-1:0]  xy_val_ff;  
+
+  // APB decoding
+  always_comb begin
+    apb_write       = apb_psel_i & apb_pwrite_i;
+
+    apb_sel_roi_val = ( apb_paddr_i == ADDR_ROI );                // Choosing a peripheral device (ROI)
+  end
+
+/////////
+
+
+  // APB data out
+  logic [APB_DATA_I_WIDTH-1:0] apb_dout_ff;
+  logic [APB_DATA_I_WIDTH-1:0] apb_dout_next;
+
+  // Input reg values xy
+  always_ff @( posedge clk_i or posedge arst_i ) begin
+    if ( arst_i ) xy_val_ff   <= '0;
+    else          xy_val_ff   <= apb_pwdata_i;
+  end
+
+
+
+  // Output reg values
+  assign apb_dout_next = apb_sel_roi_val ? ( xy_val_ff ) : ( '0 );
+
+  always_ff @( posedge clk_i or posedge arst_i ) begin
+    if ( arst_i ) apb_dout_ff <= '0;
+    else          apb_dout_ff <= apb_dout_next;
+  end
+
+
+  assign xy_0_o        = apb_dout_ff [31:0];
+  assign xy_1_o        = apb_dout_ff [63:32];
+
+endmodule
+
 */
--- src/tb/roi_apb_tb.sv
+++ src/tb/roi_apb_tb.sv
@@ -2,39 +2,114 @@
 
 module roi_apb_tb
 #(
-  parameter                     APB_DATA_I_WIDTH = 64,
-                                APB_DATA_O_WIDTH = 32,
-
-                                APB_ADDR_WIDTH = 12     // APB slaves are 4KB by default 
+  parameter                     APB_DATA_W = 32,
+                                APB_ADDR_W = 12     // APB slaves are 4KB by default 
 )();
 
-  logic                         clk_i;
-  logic                         arst_i;
+  logic                   clk_i;
+  logic                   arst_i;
 
-  logic [APB_DATA_I_WIDTH-1:0]  apb_pwdata_i;     
-  logic [APB_ADDR_WIDTH-1:0]    apb_paddr_i;          
+  logic [APB_DATA_W-1:0]  apb_pwdata_i;                
+  logic [APB_ADDR_W-1:0]  apb_paddr_i;                  
   
-  logic                         apb_pwrite_i;         
-  logic                         apb_psel_i;           
+  logic                   apb_pwrite_i;            
+  logic                   apb_psel_i;
+  logic                   apb_penable_i;  //   
 
-  logic [APB_DATA_O_WIDTH-1:0]  xy_0_o;
-  logic [APB_DATA_O_WIDTH-1:0]  xy_1_o;
+  logic  [APB_DATA_W-1:0] apb_prdata_o;   //
+  logic                   apb_pready_o;   //                
+
+  logic  [APB_DATA_W-1:0] xy_0_o;
+  logic  [APB_DATA_W-1:0] xy_1_o;
+
 
 
   roi_apb DUT_APB 
   (
-    .clk_i        ( clk_i         ),
-    .arst_i       ( arst_i        ),
+    .clk_i          ( clk_i         ),
+    .arst_i         ( arst_i        ),
 
-    .apb_pwdata_i ( apb_pwdata_i  ),
-    .apb_paddr_i  ( apb_paddr_i   ),
+    .apb_pwdata_i   ( apb_pwdata_i  ),
+    .apb_paddr_i    ( apb_paddr_i   ),
 
-    .apb_pwrite_i ( apb_pwrite_i  ),
-    .apb_psel_i   ( apb_psel_i    ),
+    .apb_pwrite_i   ( apb_pwrite_i  ),
+    .apb_psel_i     ( apb_psel_i    ),
+    .apb_penable_i  ( apb_penable_i ),
 
-    .xy_0_o       ( xy_0_o        ),
-    .xy_1_o       ( xy_1_o        )
+    .apb_prdata_o   ( apb_prdata_o  ),
+    .apb_pready_o   ( apb_pready_o  ),
+
+
+    .xy_0_o         ( xy_0_o        ),
+    .xy_1_o         ( xy_1_o        )
   );
+
+
+
+
+  ///////////
+  // TASKS //
+  ///////////
+
+
+  //  Write transfer  //
+
+  task automatic exec_apb_write_trans(
+    input logic [APB_ADDR_W-1:0] paddr,
+    input logic [APB_DATA_W-1:0] pwdata
+  );
+    // Address phase
+    apb_paddr_i   <= paddr;
+    apb_psel_i    <= 1'b1;
+    apb_pwrite_i  <= 1'b1;
+    apb_pwdata_i  <= pwdata;
+
+    // Data phase
+    @( posedge clk_i );
+    apb_penable_i <= 1'b1;
+    
+    do begin
+      @( posedge clk_i );
+    end while( !apb_pready_o ); 
+
+    // Unset penable
+    apb_penable_i <= 1'b0;
+  endtask
+
+
+
+  // Read transfer  //
+
+  task automatic exec_apb_read_trans(
+      input  bit [31:0] paddr,
+      output bit [31:0] prdata
+  );
+    // Address phase
+    apb_paddr_i  <= paddr;
+    apb_psel_i   <= 1'b1;
+    apb_pwrite_i <= 1'b0;
+
+    // Data phase
+    @( posedge clk_i );
+    apb_penable_i <= 1'b1;
+
+    do begin
+      @( posedge clk_i );
+    end while( !apb_pready_o ); 
+
+    // Save data
+    prdata = apb_prdata_o;
+    // Unset penable
+    apb_penable_i <= 1'b0;
+  endtask
+
+
+
+
+
+
+
+
 
 
    // CLK
@@ -44,28 +119,51 @@ module roi_apb_tb
   end
 
 
-// RESET
   initial begin
+   // RESET 
     arst_i = '1;
     repeat (2)  @ ( posedge clk_i );
     arst_i = '0;
+
+    exec_apb_write_trans( 12'h0, { 6'd0, 10'd200, 6'd0, 10'd200 } );
+
+
+
     repeat (20)  @ ( posedge clk_i );
     arst_i = '1;
     repeat (2)  @ ( posedge clk_i );
     arst_i = '0;
+
+    exec_apb_write_trans( 12'h0, { 6'd0, 10'd400, 6'd0, 10'd200 } );
   end
 
 
-  initial begin
-    apb_psel_i          = 1;
-    apb_pwrite_i        = 1;
 
-    apb_paddr_i         = 12'h0;
-
-    apb_pwdata_i[31:0]  = { 6'd0, 10'd200, 6'd0, 10'd200 };    
-    apb_pwdata_i[63:32] = { 6'd0, 10'd600, 6'd0, 10'd400 };
-  end
 
 
 
 endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+  initial begin
+    /*apb_psel_i          = 1;
+    apb_pwrite_i        = 1;
+
+    apb_paddr_i         = 12'h0;
+
+    apb_pwdata_i[31:0]  <= { 6'd0, 10'd200, 6'd0, 10'd200 };    
+    //apb_pwdata_i[63:32] = { 6'd0, 10'd600, 6'd0, 10'd400 };
+  end*/
